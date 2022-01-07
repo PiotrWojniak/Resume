cae322b7e2247a7d007250c9c6d83d1d2eb25e47
PiotrWojniak <wojospajder@yahoo.ie>
2022-01-07 21:43:47 +0000

    add profile imege to home.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Imports"""
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 
 from flask_pymongo import PyMongo
@@ -9,7 +9,6 @@ from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-"""Flask instance"""
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -19,13 +18,10 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-
 @app.route("/")
-#"""Test function"""
-#def test():
-#    return "Hello world"
 @app.route("/get_courses")
 def get_courses():
+    """Course render function"""
     courses = list(mongo.db.courses.find().sort("added", 1))
     courses.reverse()
     return render_template("courses.html", courses=courses)
